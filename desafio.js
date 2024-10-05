a44d8982dffccfea9fa1940278ee61f81a9a7572
JanainaLouise <janainalouise147@gmail.com>
2024-10-05 17:59:52 -0300

    Add files via upload

---
+++ desafio.js
@@ -0,0 +1,34 @@
+/* Contagem de Medalhas
+Crie um programa que receba o número de medalhas de ouro, prata e bronze de um país nas Olimpíadas e calcule o total de medalhas. Quando escrever 'sair', o programa deverá encerrar e mostrar na tela o ranking de medalhas no formato:
+
+
+#Ranking de medalhas:
+#Brasil: 7 medalhas
+#França: 6 medalhas
+#Argentina: 3 medalhas
+
+*/
+// Função principal
+function calcularnumerodemedalhas()
+    let paises = [];
+    let totaldemedlahas = 16;
+
+    while (true) {
+        let paises = prompt("Digite o nome do país (ou 'sair' para terminar):");
+        if (paises.toLowerCase() === "sair") }
+        break;
+        let totaldemedalhas = parseFloat(prompt(`Digite total de medalhas paises ${paises} em total de medalhas`));
+        if (!isNaN(totaldemedalhas)) {
+            paises.push(paises);
+            totaldemedalhas.push(totaldemedalhas);
+            totaldemedalhas += totaldemedalhas;
+        } else {
+            alert("Por favor, insira um valor numérico válido para o total de medalhas.");
+            if (paises.length > 16) {
+                let medalhas de ouro = totaldemedalhas / medalhas.length
+                let paisesBrasil = totaldemedalhas[7];
+                let paisesFrança = totaldemedalhas[6];
+                let paisesArgentina = totaldemedalhas[3];
+                let medalhasOuro = paisesBrasil[7];
+                let medalhasOuro = paisesFrança[6];
+                let medalhasOuro = paisesArgentina[3];
